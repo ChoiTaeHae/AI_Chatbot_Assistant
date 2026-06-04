0427def1b00aa95957a663d877bbeb8b17a54b59
jonghyeok-j <jjc5788@naver.com>
2026-06-04 21:05:16 +0900

    기능추가

--- BackEnd/app/core/Database.py
+++ BackEnd/app/core/Database.py
@@ -1,32 +1,33 @@
 # app/core/database.py
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
 DATABASE_URL = (
-    f"postgresql://{settings.DB_USER}:"
+    f"postgresql+asyncpg://{settings.DB_USER}:"
     f"{settings.DB_PASSWORD}@"
     f"{settings.DB_HOST}:"
     f"{settings.DB_PORT}/"
     f"{settings.DB_NAME}"
 )
 
-engine = create_engine(
+engine = create_async_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(
+AsyncSessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
-    bind=engine
+    bind=engine,
+    class_=AsyncSession
 )
 
 Base = declarative_base()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+async def get_db():
+    async with AsyncSessionLocal() as db :
+        try:
+            yield db
+        finally:
+            await db.close()
