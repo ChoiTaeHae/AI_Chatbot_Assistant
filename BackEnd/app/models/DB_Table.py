6d424ae757770de59421d96e24c5c92b79c85a2a
jonghyeok-j <jjc5788@naver.com>
2026-06-08 15:39:38 +0900

    0123

--- BackEnd/app/models/DB_Table.py
+++ BackEnd/app/models/DB_Table.py
@@ -25,7 +25,7 @@ class Student(Base):
     dept_id = Column(Integer, ForeignKey("department.id"))
     student_no = Column(String, unique=True, nullable=False)  # 학번
     password_hash = Column(String, nullable=False)            # hashed login
-    name = Column(String, nullable=False)                     # 입학년도/이름
+    name = Column(String, nullable=False)                     # 이름
     interests = Column(JSON)                                  # 관심사 태그 (JSONB 대응)
 
 # ==========================================
